757d9b6de28c74db24215da5c3a5e2456acd09fb
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-04-15 12:56:02 -0500

    Update src/index.js to go through element example on reactjs.org

--- src/index.js
+++ src/index.js
@@ -4,8 +4,28 @@ import './index.css';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+// ReactDOM.render(
+// 	<h1>Hello World</h1>,
+// 	document.getElementById('root')
+// );
+
+function formatName(user) {
+	return user.firstName + ' ' + user.lastName;
+}
+
+const user = {
+	firstName: "Jeremy",
+	lastName: "Ward"
+};
+
+const element = (
+	<h1>
+		Hello, {formatName(user)}!
+	</h1>
+);
+
 ReactDOM.render(
-	<h1>Hello World</h1>,
+	element,
 	document.getElementById('root')
 );
 registerServiceWorker();
