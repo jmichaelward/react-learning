dbebcf08d96b2d087e03b484fdca11b956a5ee59
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-04-15 13:58:45 -0500

    Set up Clock ticket example the 'wrong' way.

--- src/index.js
+++ src/index.js
@@ -2,38 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 //import App from './App';
-import Comment from './component/Comment';
+import Clock from './component/Clock';
 import registerServiceWorker from './registerServiceWorker';
 
+function tick() {
+	ReactDOM.render(
+		<Clock date={new Date()} />,
+		document.getElementById('root')
+	);
+}
 
+setInterval(tick, 1000);
 
-
-
-const comment = {
-	date: new Date(),
-	text: 'I hope you enjoy learning React!',
-	author: {
-		name: 'Hello Kitty',
-		avatarUrl: 'http://placekitten.com/g/64/64',
-	},
-};
-
-ReactDOM.render(
-	<Comment
-		date={comment.date}
-		text={comment.text}
-		author={comment.author}
-	/>,
-	document.getElementById('root')
-);
-
-
-ReactDOM.render(
-		<Comment
-			date={comment.date}
-			text={comment.text}
-			author={comment.author}
-		/>,
-	document.getElementById('root')
-);
 registerServiceWorker();
