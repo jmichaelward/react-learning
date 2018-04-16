6f97ceba505ff98a00f860d15e3d29507078d536
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-04-15 22:11:03 -0500

    Implement NameForm component and  output within app.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import NumberList from './component/NumberList';
+import NameForm from './component/NameForm';
 import './index.css';
 //import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-const numbers = [1, 2, 3, 4, 5];
-
 ReactDOM.render(
-	<NumberList numbers={numbers} />,
+	<NameForm />,
 	document.getElementById('root')
 );
 
