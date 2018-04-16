ab8339dd185c0eae00d4b6c7dd2d061b33f70def
Jeremy Ward <jeremy.ward@webdevstudios.com>
2018-04-15 20:47:51 -0500

    Add the ListItem component and update the key to be applied inside the component creation.

--- src/component/NumberList.js
+++ src/component/NumberList.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import ListItem from './ListItem';
 
 class NumberList extends Component {
 	constructor(props) {
 		super(props);
 		this.numbers = props.numbers;
+
+		// A good rule of thumb is that elements inside the `map()` call need keys.
 		this.listItems = this.numbers.map((number) =>
-			<li>{number}</li>
+			<ListItem key={number.toString()} value={number} />
 		);
 	}
 	render() {
